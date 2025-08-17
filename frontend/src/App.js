c9fd76c05da204fca90591fb8225937be6837d16
smirk-dev <suryansh.mishra.sm6@gmail.com>
2025-08-18 00:16:19 +0530

    Add main application structure with routing and layout components

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import UploadPage from './pages/UploadPage';
+import EditorPage from './pages/EditorPage';
+import AnalysisPage from './pages/AnalysisPage';
+import ProjectsPage from './pages/ProjectsPage';
+
+function App() {
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <Navbar />
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/upload" element={<UploadPage />} />
+          <Route path="/editor" element={<EditorPage />} />
+          <Route path="/analysis" element={<AnalysisPage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+        </Routes>
+      </Box>
+    </Box>
+  );
+}
+
+export default App;
